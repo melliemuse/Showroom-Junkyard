20313d0487110f015d4fc0aa8253bb102cf89cf7
Melody Stern <melliemuse@gmail.com>
2020-01-14 13:56:19 -0600

    added intersection method for junkyard and showroom sets

--- cars.py
+++ cars.py
@@ -22,6 +22,6 @@ print(showroom)
 # Now create another set of cars in a variable junkyard. Someone who owns a junkyard full of old cars has approached you about buying the entire inventory. In the new set, add some different cars, but also add a few that are the same as in the showroom set.
 junkyard = {"old Prius", "Vintage", "rustbucket", "clunker", "Spyder"}
 # Use the intersection method to see which cars exist in both the showroom and that junkyard.
-
+overlap = junkyard.intersection(showroom)
 # Now you're ready to buy the cars in the junkyard. Use the union method to combine the junkyard into your showroom.
 # Use the discard() method to remove any cars that you acquired from the junkyard that you do not want in your showroom.
